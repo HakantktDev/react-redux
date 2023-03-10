9be63849ad8f9667160013a8506e7826dc6ceec3
Hakan Tokat <hakantokatdev@gmail.com>
2023-03-10 17:10:12 +0300

    migrating-everything-to-redux-toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -13,9 +13,9 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
-    togglecounter(state) {
+    toggleCounter(state) {
       state.showCounter = !state.showCounter;
     },
   },
@@ -26,4 +26,5 @@ const store = configureStore({
   reducer: counterSlice.reducer,
 });
 
+export const counterActions = counterSlice.actions;
 export default store;
